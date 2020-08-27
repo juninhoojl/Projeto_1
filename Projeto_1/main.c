f818b0f0eeda8767b6eb7737d79e6c6771998dea
José Luiz Corrêa Junior <juninhopc@icloud.com>
2020-08-27 16:47:45 -0300

    Initial Commit

---
+++ Projeto_1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Projeto_1
+//
+//  Created by José Luiz Junior on 27/08/20.
+//  Copyright © 2020 José Luiz Junior. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
